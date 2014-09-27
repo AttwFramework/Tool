659fe4ae8f2876fbf1927cd5b3eb1f230c8736aa
GabrielJMJ <gamjj74@hotmail.com>
2014-09-27 17:14:23 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Tool
+=====
+[![Total Downloads](https://poser.pugx.org/attwframework/tool/downloads.png)](https://packagist.org/packages/attwframework/tool) [![Latest Unstable Version](https://poser.pugx.org/attwframework/tool/v/unstable.png)](https://packagist.org/packages/attwframework/tool) [![License](https://poser.pugx.org/attwframework/tool/license.png)](https://packagist.org/packages/attwframework/tool)
+
+Useful tools of [AttwFramework](https://github.com/attwframework/framework).
---
+++ UrlParser.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * AttwFramework
+ *
+ * @author Gabriel Jacinto <gamjj74@hotmail.com>
+ * @license MIT License
+ * @link http://attwframework.github.io
+*/
+
+namespace Attw\Tool;
+
+use  Attw\Tool\Exception\UrlParserException;
+
+class UrlParser
+{
+    /**
+     * Return the query strings from a URL
+     *
+     * @param string $url
+     * @return array Queries
+    */
+    public function getQueries($url)
+    {
+        $this->verifyUrl($url);
+        $params = explode('?', $url);
+        $queries = array();
+
+        if (count($params) > 1) {
+            $params = end($params);
+            $relations = explode('&', $params);
+
+            $queries = array();
+
+            foreach ($relations as $relation) {
+                $camps = explode('=', $relation);
+                $queries[ $camps[0] ] = $camps[1];
+            }
+        }
+
+        return $queries;
+    }
+
+    /**
+     * @param string $url
+    */
+    private function verifyUrl($url)
+    {
+        if (!filter_Var($url, FILTER_VALIDATE_URL)) {
+            UrlParserException::invalidUrl($url);
+        }
+    }
+}
